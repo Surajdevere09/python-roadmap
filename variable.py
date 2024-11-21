238c99de9eabc1c3c008c32dc85c377297b90aad
Surajdevere09 <suraj.devere@gmail.com>
2024-11-21 09:14:00 +0530

    variable added

---
+++ variable.py
@@ -0,0 +1,26 @@
+x=5
+y="John"
+
+print(x)
+
+# Casting
+z = str(5)
+print(z)
+print(type(z))
+
+
+#Assining multiple values
+
+x,y,z = "Orange","Banana", "Cherry"
+
+x=y=z="Lemon"
+print(x)
+print(y)
+print(z)
+
+
+# unpacking
+fruits = ["Orange","Banana", "Cherry"]
+
+x,y,z =fruits
+print(x,y,z)
